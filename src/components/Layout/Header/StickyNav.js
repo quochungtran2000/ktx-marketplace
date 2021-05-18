8058518238c0bb5ee327b55c2b9ddbd418992992
Tan Loi <80636250+LoiKise@users.noreply.github.com>
2021-05-18 15:06:24 +0700

    Upload source Reponsive

--- src/components/Layout/Header/StickyNav.js
+++ src/components/Layout/Header/StickyNav.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { } from 'react';
 import { Link } from 'react-router-dom'
 const StickyNav = () => {
   const logoUrl = `https://upload.wikimedia.org/wikipedia/vi/c/c6/Logo_KTX_%C4%90HQGTPHCM.png`
   return (
     <header className="header">
+
       <Link className="header__logo" to="/">
-        <img src={logoUrl} alt="ktx-marketplace-logo" width={70}/>
+        <img src={logoUrl} alt="ktx-marketplace-logo" width={70} />
       </Link>
-        <nav className="header__nav">
-          <ul className="nav__links">
-            <li>
-              {/* <Link to={"/"}><a href="/">ád</a></Link> */}
-              <Link className="nav__link" to="/">Trang chủ</Link>
-            </li>
-            <li>
-              <Link className="nav__link" to="/news">Tin Tức</Link>
-            </li>
-            <li>
-              <Link className="nav__link" to="/communicate">Cộng Đồng</Link>
-            </li>
-            <li>
-              <Link className="nav__link" to="/contact">Liên Hệ</Link>
-            </li>
-          </ul>
-        </nav>
-        <button className="nav__link header__signin">Đăng Nhập</button>
-      </header>
+      <nav className="header__nav">
+        <ul className="nav__links">
+          <li>
+            {/* <Link to={"/"}><a href="/">ád</a></Link> */}
+            <Link className="nav__link" to="/">Trang chủ</Link>
+          </li>
+          <li>
+            <Link className="nav__link" to="/news">Tin Tức</Link>
+          </li>
+          <li>
+            <Link className="nav__link" to="/communicate">Cộng Đồng</Link>
+          </li>
+          <li>
+            <Link className="nav__link" to="/contact">Liên Hệ G</Link>
+          </li>
+
+        </ul>
+      </nav>
+      <button className="nav__link header__signin">Đăng Nhập</button>
+    </header>
   );
 }
 
